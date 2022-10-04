ea3f8365e617a0870018d6a3ff474c9af08303dd
Ykyki4 <jimmypantalones@gmail.com>
2022-10-04 16:30:31 +0300

    Download book cover images and get comments.

--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
 from pathlib import Path, PurePath
+from urllib.parse import urljoin, urlsplit
+
 from bs4 import BeautifulSoup
 import requests
 from requests import HTTPError
@@ -26,15 +28,34 @@ def download_txt(url, filename, folder='books/'):
     return path
 
 
+def download_book_cover(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    Path("images").mkdir(parents=True, exist_ok=True)
+    filename = urlsplit(response.url).path.split("/")[2]
+    path = PurePath("images", filename)
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
+
 def parse_book(response):
+    comments = []
     soup = BeautifulSoup(response.text, 'lxml')
-    try:
-        heading_text = soup.find('div', id='content').find('h1').text
-    except AttributeError:
-        return
+    heading_text = soup.find('div', id='content').find('h1').text
+    image = soup.find('div', class_='bookimage').find('img')['src']
+    texts_tags = soup.find_all('div', class_='texts')
+    for tag in texts_tags:
+        comments.append(tag.find('span', class_='black').text)
     book_title = heading_text.split("::")[0].strip()
     author = heading_text.split("::")[1].strip()
-    return book_title
+    image_url = urljoin('https://tululu.org', image)
+    book = {
+        "title": book_title,
+        "author": author,
+        "image": image_url,
+        "comments": comments
+    }
+    return book
 
 
 def get_book():
@@ -42,10 +63,18 @@ def get_book():
         parse_url = f"https://tululu.org/b{id+1}/"
         response = requests.get(parse_url)
         response.raise_for_status()
-        book_name = parse_book(response)
+        try:
+            check_for_redirect(response)
+        except HTTPError:
+            print("Книги не существует")
+            continue
+        book = parse_book(response)
+        book_name = book["title"]
+        img_url = book["image"]
         txt_url = f"https://tululu.org/txt.php?id={id+1}"
         filename = f'{id+1} {book_name}'
         download_txt(txt_url, filename)
+        download_book_cover(img_url)
 
 
 if __name__ == "__main__":
