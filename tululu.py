e12b3bbe88da65600bb23036b8e1502fcb5d290b
Ykyki4 <jimmypantalones@gmail.com>
2022-10-05 12:04:01 +0300

    Update tululu.py
    Make a exception for ConnectionError, switch loops to comprehensions, remove for unpack in heading_text

--- tululu.py
+++ tululu.py
@@ -5,7 +5,8 @@ from bs4 import BeautifulSoup
 import requests
 from requests import HTTPError
 from pathvalidate import sanitize_filename
-
+import sys
+import time
 
 def check_for_redirect(response):
     if response.url == "https://tululu.org/":
@@ -43,54 +44,57 @@ def download_book_cover(url):
 
 
 def parse_book(response):
-    comments = []
-    book_genres = []
+
     soup = BeautifulSoup(response.text, 'lxml')
     heading_text = soup.find('div', id='content').find('h1').text
     image = soup.find('div', class_='bookimage').find('img')['src']
     texts_tags = soup.find_all('div', class_='texts')
-    for tag in texts_tags:
-        comments.append(tag.find('span', class_='black').text)
+    comments = [tag.find('span', class_='black').text for tag in texts_tags]
     book_genres_tags = soup.find('span', class_='d_book').find_all("a")
-    for book_genre in book_genres_tags:
-        book_genres.append(book_genre.text)
-    book_title = heading_text.split("::")[0].strip()
-    author = heading_text.split("::")[1].strip()
+    book_genres = [book_genre.text for book_genre in book_genres_tags]
     image_url = urljoin('https://tululu.org', image)
+    title, author = heading_text.split("::")
     book = {
-        "title": book_title,
+        "title": title,
         "author": author,
         "image": image_url,
         "comments": comments,
         "genre": book_genres
     }
+
+        # book["title"] = title.strip()
+        # book["author"] = author.strip()
     return book
 
 
 def get_books():
     args = parse_args()
     for book_id in range(args.start_id, args.end_id):
-        parse_url = f"https://tululu.org/b{book_id}/"
-        response = requests.get(parse_url)
-        response.raise_for_status()
         try:
-            check_for_redirect(response)
-        except HTTPError:
-            print("Книги не существует")
-            continue
-        book = parse_book(response)
-        book_name = book["title"]
-        img_url = book["image"]
-        txt_url = f"https://tululu.org/txt.php?id={book_id}"
-        filename = f'{book_id} {book_name}'
-        try:
-            download_txt(txt_url, filename)
-        except HTTPError:
-            print("Книги не существует")
-            continue
-        download_book_cover(img_url)
-        print(f"Название книги: {book_name}")
-        print(book['genre'])
+            parse_url = f"https://tululu.org/b{book_id}/"
+            response = requests.get(parse_url)
+            response.raise_for_status()
+            try:
+                check_for_redirect(response)
+            except HTTPError:
+                print("Книги не существует")
+                continue
+            book = parse_book(response)
+            book_name = book["title"]
+            img_url = book["image"]
+            txt_url = f"https://tululu.org/txt.php?id={book_id}"
+            filename = f'{book_id} {book_name}'
+            try:
+                download_txt(txt_url, filename)
+            except HTTPError:
+                print("Книги не существует")
+                continue
+            download_book_cover(img_url)
+            print(f"Название книги: {book_name}")
+            print(book['genre'])
+        except requests.exceptions.ConnectionError:
+            print("Connection lost, next try in 1 minute", file=sys.stderr)
+            time.sleep(60)
 
 
 if __name__ == "__main__":
