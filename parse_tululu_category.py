d7bb86f0195c97e634c84d0539b0bcd816c2d46a
Ykyki4 <jimmypantalones@gmail.com>
2022-10-08 21:22:04 +0300

    Integrate livereload.

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -85,9 +85,11 @@ def get_books():
                 txt_url = f"https://tululu.org/txt.php"
                 filename = f'{book_name}'
                 if not args.skip_txt:
-                    book["book_path"] = str(download_txt(txt_url, params, filename, download_path))
+                    book_path, book_filename = os.path.split(download_txt(txt_url, params, filename, download_path))
+                    book['book_path'] = os.path.join("books" + os.sep + book_filename)
                 if not args.skip_imgs:
-                    book["img_src"] = str(download_book_cover(img_url, download_path))
+                    img_path, img_filename = os.path.split(download_book_cover(img_url, download_path))
+                    book['img_src'] = os.path.join("images" + os.sep + img_filename)
                 books.append(book)
             except requests.exceptions.ConnectionError:
                 print("Connection lost, next try in 1 minute", file=sys.stderr)
--- render_website.py
+++ render_website.py
@@ -2,24 +2,33 @@ import json
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from parse_tululu_category import parse_args
+from livereload import Server, shell
 
-args = parse_args()
+def on_reload():
+    args = parse_args()
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-template = env.get_template('template.html')
+    template = env.get_template('template.html')
 
-with open(args.json_path, "r", encoding="utf-8") as my_file:
-    books_json = my_file.read()
+    with open(args.json_path, "r", encoding="utf-8") as my_file:
+        books_json = my_file.read()
 
-books = json.loads(books_json)
+    books = json.loads(books_json)
 
-rendered_page = template.render(
-    books=books
-)
+    rendered_page = template.render(
+        books=books
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+if __name__ == "__main__":
+
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
