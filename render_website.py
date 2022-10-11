354f73255fa7b93a9a6f3b04ebb5b537e9c0e52b
Ykyki4 <jimmypantalones@gmail.com>
2022-10-11 12:11:18 +0300

    Multi changes
    Change json.loads to load.
    Encoded urls to images and books.
    Sorted imports.
    Make variables to chuncked.
    Add a start numb to enumerate

--- render_website.py
+++ render_website.py
@@ -1,13 +1,11 @@
 import json
 import math
-import os
-from itertools import count
 from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from parse_tululu_category import parse_args
-from livereload import Server, shell
+from livereload import Server
 from more_itertools import chunked
+from parse_tululu_category import parse_args
 
 def on_reload():
     args = parse_args()
@@ -20,17 +18,19 @@ def on_reload():
     template = env.get_template('template.html')
 
     with open(args.json_path, "r", encoding="utf-8") as my_file:
-        books_json = my_file.read()
+        books = json.load(my_file)
 
-    books = json.loads(books_json)
-    for page, books_page_chunks in enumerate(list(chunked(books, 10))):
+    page_chunk = 10
+    col_chunk = 2
+
+    for page, books_page_chunks in enumerate(list(chunked(books, page_chunk)), start=1):
         rendered_page = template.render(
-            books_chunks=list(chunked(books_page_chunks, 2)),
-            pages_numb=math.ceil(len(books) / 10),
-            current_page=page+1
+            books_chunks=list(chunked(books_page_chunks, col_chunk)),
+            pages_numb=math.ceil(len(books) / page_chunk),
+            current_page=page
         )
         Path('pages').mkdir(parents=True, exist_ok=True)
-        with open(f'pages/index{page+1}.html', 'w', encoding="utf8") as file:
+        with open(f'pages/index{page}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
 
 
